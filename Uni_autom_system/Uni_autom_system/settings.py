c25e91ded0e110531e37875d84202a580acbed60
Mudassar Amin <mudasiramin456@gmail.com>
2023-05-18 11:21:25 +0500

    commit

--- Uni_autom_system/Uni_autom_system/settings.py
+++ Uni_autom_system/Uni_autom_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Uni_autom_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
